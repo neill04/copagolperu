6bb4194a8067355f34a444c68652c07e36c2fa12
neill04 <olazabalneill2004@gmail.com>
2025-08-12 23:53:38 -0500

    First commit, project initialization

---
+++ src/main/java/com/neill/copagolperu/CopagolperuApplication.java
@@ -0,0 +1,13 @@
+package com.neill.copagolperu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopagolperuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopagolperuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neill/copagolperu/CopagolperuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neill.copagolperu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopagolperuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
